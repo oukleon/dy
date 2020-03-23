1159bae0136273ddee7e65e444623d4ce67f6000
oukleon <54334941+oukleon@users.noreply.github.com>
2020-03-22 20:41:06 -0400

    Add files via upload

---
+++ kv_test3.py
@@ -0,0 +1,194 @@
+# source ~/kivy_venv/bin/activate
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.graphics import Color, Rectangle
+from kivy.config import Config
+from kivy.core.clipboard import Clipboard
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.scrollview import ScrollView
+import requests
+
+# from kivy.lang import Builder
+# Builder.load_file("exer.kv")
+
+# https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=8123+4493+6011&sources=ONCHigh
+
+Config.set('graphics', 'width', '500')
+Config.set('graphics', 'height', '500')
+
+#Repositories
+drug_list1=[]
+drug_rxcui_list=[]
+address_string=""
+sev=[]
+inter=[]
+final_list=[]
+full_drug_list=[]
+interaction={}
+
+
+class MyTextInput(TextInput):
+    def keyboard_on_key_down(self, window, keycode, text, modifiers):
+        """ Add support for tab as an 'autocomplete' using the suggestion text.
+        """
+    
+        if self.suggestion_text and keycode[1] == 'tab':
+            self.insert_text(self.suggestion_text + ' ')
+            return True
+        return super(MyTextInput, self).keyboard_on_key_down(window, keycode, text, modifiers)
+
+class MainGrid(Widget):
+    
+
+    drug1 = ObjectProperty(None)
+    drug_list = ObjectProperty(None)
+    desc = ObjectProperty(None)
+    
+    def pressed_add(self):
+        if not self.drug1.text:
+            pass
+
+        else:
+            global drug_list1 
+            drug_list1.append(self.drug1.text)
+            self.drug_list.text += self.drug1.text +'\n'
+            self.drug1.text=""
+
+      
+        
+    def pressed_re(self):
+        full_drug_list.clear()
+        drug_list1.clear() 
+        drug_rxcui_list.clear()
+        address_string=""
+        self.drug1.text=""
+        self.drug_list.text=""
+        self.desc.text=""
+        sev.clear()
+        inter.clear()
+        final_list.clear()
+        printing=""
+        strin=""
+        num_desc=""
+        url2=""
+        url3=""
+        interaction.clear()
+
+
+    def pressed(self):
+        final_list.clear()
+        self.desc.text=""
+        def check_interaction(drug_list1):
+            if not drug_list1:
+               return "You should put at least one drug."
+            
+            # if "" in drug_list1:
+            #     return "You should put at least one drug."
+            #     pressed_re()
+
+            for i in drug_list1:
+                
+                url2=f"https://rxnav.nlm.nih.gov/REST/approximateTerm?term={i}"
+                rxcui2=requests.get(url2, headers={"Accept":"application/json"})
+
+                drug_data=rxcui2.json()
+
+                if 'candidate' not in drug_data["approximateGroup"]:
+                    return "The drug(s) cannot be found."
+                else:
+                    drug_rxcui=drug_data["approximateGroup"]['candidate'][0]['rxcui']
+                    global drug_rxcui_list
+                    drug_rxcui_list.append(drug_rxcui)
+            
+            global address_string 
+            address_string += drug_rxcui_list[0]
+            for i in drug_rxcui_list[1:]:
+                address_string += f"+ {i}"
+
+            url3=f"https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis={address_string}"
+            response=requests.get(url3, headers={"Accept":"application/json"})
+            interaction=response.json()
+            print(type(response), type(interaction),)
+
+          
+            if 'fullInteractionTypeGroup' not in interaction:
+                return 'No interaction'
+            
+            else:                    
+                num_desc=len(interaction['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'])
+                
+                for i in range(0,num_desc):
+                    final_severity=interaction['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][i]['severity']
+                    final_interaction=interaction['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][i]['description']
+                    global sev
+                    global inter
+                    sev.append(final_severity)
+                    inter.append(final_interaction)
+                    
+
+                for i in range(num_desc):
+                    printing=f"Severity: {sev[i]}, Drug interaction: {inter[i]}"
+                    global final_list
+                    final_list.append(printing)
+                return str(final_list)
+
+                   
+    
+            # else:
+            #     final_severity=interaction['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][0]['severity']
+            #     final_interaction=interaction['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][0]['description']
+                
+          
+
+
+         
+        strin=check_interaction(drug_list1)
+        self.desc.text=strin
+        # drug_list1.clear()
+
+        # drug_list.text=""
+
+        # print(self.desc.text)
+        # print(type(self.desc))
+        # desc = ObjectProperty(check_interaction(self.drug1.text,self.drug2.text))
+        # print(self.drug1.text)
+        # check_interaction(self.drug1.text,self.drug2.text)
+        # self.count += 1
+        # print(self.drug1.text, self.count)
+    
+
+
+
+    def copyText(self):
+        show_popup()
+        Clipboard.copy(self.desc.text)
+
+
+class CopiedPopUp(FloatLayout):
+    pass
+
+def show_popup():
+    show = CopiedPopUp()
+    window = Popup(title="Copied", content=show, size_hint=(None,None), size=(300,100), auto_dismiss= True) 
+    window.open()
+    def dismiss_popup(dt):
+        print(dt)
+        window.dismiss()
+    Clock.schedule_once(dismiss_popup, 1)
+
+class ExerApp(App):
+    def build(self):
+        return MainGrid()
+
+if __name__ == "__main__":
+    ExerApp().run()
+
+
